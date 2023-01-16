326ebd652c422d1a9c87275542da2d7fd2227aa7
GeorgeCodeHub <54906099+GeorgeCodeHub@users.noreply.github.com>
2023-01-16 12:33:31 +0200

    Change app structure for Railway

--- server/main.py
+++ server/main.py
@@ -1,5 +1,7 @@
+import uvicorn
 from fastapi import FastAPI
 from fastapi.staticfiles import StaticFiles
+
 
 from .routers import books, search
 
@@ -15,3 +17,11 @@ app.include_router(search.router)
 
 # Load frontend at root of the server
 app.mount("/", StaticFiles(directory="server/build/", html=True), name="static")
+
+if __name__ == "__main__":
+    uvicorn.run(
+        "main:app",
+        host="0.0.0.0",
+        port=os.getenv("PORT", default=5000),
+        log_level="info",
+    )
